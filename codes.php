4e9c9f6c11d6ae91992cea2662aed991beb4262a
Geejay09 <jomarialmario580@gmail.com>
2025-05-06 16:29:15 +0800

    .

--- codes.php
+++ codes.php
@@ -8,7 +8,7 @@ $result = $conn->query("SELECT * FROM tbl_items WHERE deleted = 0");
 <head>
     <meta charset="UTF-8">
     <title>Stock Items</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.mins.css" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
    
